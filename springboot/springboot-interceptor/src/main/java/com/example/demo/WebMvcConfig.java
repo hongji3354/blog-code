643a74bbbda740b3280d884b7fa1a7af3f01a953
Ji Hun Hong <hongji3354@gmail.com>
2020-02-15 21:50:58 +0900

    springboot interceptor 예제 구현

---
+++ springboot/springboot-interceptor/src/main/java/com/example/demo/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Resource(name = "userInterceptor")
+    private HandlerInterceptor userInterceptor;
+
+    @Resource(name = "adminInterceptor")
+    private HandlerInterceptor adminInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userInterceptor)
+                .addPathPatterns("/user");
+        registry.addInterceptor(adminInterceptor)
+                .addPathPatterns("/admin");
+    }
+}
